a4ddc9127a477e49b0f27bdd960177a85ab9f347
Shefali-singh-OCT <shefalisinghb@gmail.com>
2024-09-10 17:59:29 +0530

    created user model

---
+++ Models/UserModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: email,
+        required: true,
+        unique: true
+    },   
+    password: {
+        type: String,
+        required: true
+    }   
+},{ timestamps: true })
+
+const User = mongoose.model("User",Schema)
+export default User
