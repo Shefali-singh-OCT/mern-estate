f3935c61debeffd905a00231c8069e6f44b13597
Shefali-singh-OCT <shefalisinghb@gmail.com>
2024-09-20 02:16:04 +0530

    create user update api

--- API/controller/userController.js
+++ API/controller/userController.js
@@ -1,7 +1,31 @@
+import User from "../../Models/UserModel.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs'
+
 export const test = (req, res, next) => {
   res.json({ 
     message: "api router is working", 
     name: "Shefali singh" });
 };
 
+export const updateUser = async (req,res,next) => {
+   if(req.user.is !== req.params.id) return next(errorHandler(401, 'You can only update your own account'))
+    try{
+             if(req.body.password){
+              req.body.password = bcryptjs.hashSync(req.body.password,10);
+             }
+             const updateUser = await User.findByIdAndUpdate(req.params.id ,{
+              $set: {
+                username:req.body.username,
+                email:req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+              }
+             },{new : true})
+             const {password: pass, ...rest} = updateUser._doc
 
+             res.status(200).json(rest);
+    }catch{error}{
+      next(error)
+    }
+}
--- API/routes/user-route.js
+++ API/routes/user-route.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { test } from '../controller/userController.js';
+import { test, updateUser } from '../controller/userController.js';
+import { verifyToken } from '../utils/verifyuser.js';
 
 const router = express.Router();
 router.get("/test",test);
+router.post("/update/:id",verifyToken,updateUser)
 
 export default router
