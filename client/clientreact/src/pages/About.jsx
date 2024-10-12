fdf0e0e43682ccad63595c8b2f1ee8d61a69e0af
Shefali-singh-OCT <shefalisinghb@gmail.com>
2024-10-13 05:12:20 +0530

    completed about page

--- client/clientreact/src/pages/About.jsx
+++ client/clientreact/src/pages/About.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>
-      about
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-slate-700 text-3xl font-bold mb-4">About Shefali Estate</h1>
+      <p className="mb-4 text-slate-700 ">
+        Shefali Estate is a leading real estate agency that specializes in
+        helping clients buy, sell ans rent properties in teh most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providibg
+        exceptional services and making the buying and selling process as smooth
+        as possible.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Our mission is to help out clients achieve their real estate goals by
+        providing expert advice, personalized servisec and a deep understanding
+        of the local market. Wether you are locking to buy, sell or rent a
+        property, we are here to help you every step of teh way.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Our teams of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committted to providing the highest level of
+        service to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
